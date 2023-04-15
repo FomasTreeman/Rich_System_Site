6ba14a96773ebc53da29c1924a7b57673ba0076e
FomasTreeman <tom@team-freeman.com>
2023-04-15 13:05:21 +0100

    reg

--- src/routes/graph/+page.svelte
+++ src/routes/graph/+page.svelte
@@ -25,12 +25,38 @@
   function log() {
     type = xy().map((day) => ({ x: day.x, y: Math.log(day.y) }));
   }
+
+  function reg() {
+    const oldData = xy();
+    const newData = oldData.map((coord) => {
+      return { x: Math.floor(Date.parse(coord.x) / 1000000), y: coord.y };
+    });
+    console.log(newData);
+    const n = newData.length;
+    let xSum = 0,
+      ySum = 0,
+      xySum = 0,
+      xSqSum = 0;
+    for (let i = 0; i < n; i++) {
+      xSum += newData[i].x;
+      ySum += newData[i].y;
+      xySum += newData[i].x * newData[i].y;
+      xSqSum += newData[i].x * newData[i].x;
+    }
+    const slope = (n * xySum - xSum * ySum) / (n * xSqSum - xSum * xSum);
+    const yIntercept = (ySum - slope * xSum) / n;
+    type = newData.map((day, i) => ({
+      x: oldData[i].x,
+      y: slope * day.x + yIntercept,
+    }));
+  }
 </script>
 
 <nav>
   <button on:click={profit}>PROFIT</button>
   <button on:click={kitty}>KITTY</button>
-  <button on:click={log}>LOG</button>
+  <!-- <button on:click={log}>LOG</button> -->
+  <button on:click={reg}>REG</button>
 </nav>
 <Chart {type} />
 
