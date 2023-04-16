ba8420d76509da00cce29766be382e6d0cbbfa20
FomasTreeman <tom@team-freeman.com>
2023-04-16 23:43:46 +0100

    reg on all graphs

--- src/routes/graph/+page.svelte
+++ src/routes/graph/+page.svelte
@@ -15,47 +15,18 @@
 
   function kitty() {
     let acc = 840;
-    type = xy().map((day) => ({ x: day.x, y: (acc += day.y) }));
-  }
-
-  function profit() {
-    type = xy();
+    return xy().map((day) => ({ x: day.x, y: (acc += day.y) }));
   }
 
   function log() {
-    type = xy().map((day) => ({ x: day.x, y: Math.log(day.y) }));
-  }
-
-  function reg() {
-    const oldData = xy();
-    const newData = oldData.map((coord) => {
-      return { x: Math.floor(Date.parse(coord.x) / 1000000), y: coord.y };
-    });
-    const n = newData.length;
-    let xSum = 0,
-      ySum = 0,
-      xySum = 0,
-      xSqSum = 0;
-    for (let i = 0; i < n; i++) {
-      xSum += newData[i].x;
-      ySum += newData[i].y;
-      xySum += newData[i].x * newData[i].y;
-      xSqSum += newData[i].x * newData[i].x;
-    }
-    const slope = (n * xySum - xSum * ySum) / (n * xSqSum - xSum * xSum);
-    const yIntercept = (ySum - slope * xSum) / n;
-    type = newData.map((day, i) => ({
-      x: oldData[i].x,
-      y: slope * day.x + yIntercept,
-    }));
+    return xy().map((day) => ({ x: day.x, y: Math.log(day.y) }));
   }
 </script>
 
 <nav>
-  <button on:click={profit}>PROFIT</button>
-  <button on:click={kitty}>KITTY</button>
-  <button on:click={log}>LOG</button>
-  <button on:click={reg}>REG</button>
+  <button on:click={() => (type = xy())}>PROFIT</button>
+  <button on:click={() => (type = kitty())}>KITTY</button>
+  <button on:click={() => (type = log())}>LOG</button>
 </nav>
 <Chart {type} />
 
--- src/routes/graph/Chart.svelte
+++ src/routes/graph/Chart.svelte
@@ -3,13 +3,36 @@
   import Chart from "chart.js/auto";
 
   export let type;
-
   let ctx;
   let myChart;
 
   $: if (myChart) {
     myChart.data.datasets[0].data = type;
+    myChart.data.datasets[1].data = regression(type);
     myChart.update();
+  }
+
+  function regression(oldData) {
+    const newData = oldData.map((coord) => {
+      return { x: Math.floor(Date.parse(coord.x) / 1000000), y: coord.y };
+    });
+    const n = newData.length;
+    let xSum = 0,
+      ySum = 0,
+      xySum = 0,
+      xSqSum = 0;
+    for (let i = 0; i < n; i++) {
+      xSum += newData[i].x;
+      ySum += newData[i].y;
+      xySum += newData[i].x * newData[i].y;
+      xSqSum += newData[i].x * newData[i].x;
+    }
+    const slope = (n * xySum - xSum * ySum) / (n * xSqSum - xSum * xSum);
+    const yIntercept = (ySum - slope * xSum) / n;
+    return newData.map((day, i) => ({
+      x: oldData[i].x,
+      y: slope * day.x + yIntercept,
+    }));
   }
 
   onMount(async () => {
@@ -21,6 +44,10 @@
             label: "days",
             data: type,
             borderWidth: 1,
+          },
+          {
+            label: "regression",
+            pointRadius: 0,
           },
         ],
       },
