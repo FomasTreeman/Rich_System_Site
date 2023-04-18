bc3e4804c4b5a067c46ad6b3ff1d7174df724a4e
FomasTreeman <tom@team-freeman.com>
2023-04-18 14:48:16 +0100

    state fix + filter for all

--- src/routes/graph/+page.svelte
+++ src/routes/graph/+page.svelte
@@ -2,7 +2,7 @@
   import Chart from "./Chart.svelte";
 
   export let data;
-  let graphType = "xy";
+  let graphType = xy;
   let type = xy();
 
   function xy() {
@@ -30,54 +30,47 @@
     return number;
   }
 
-  function filterKitty(e) {
-    return kitty().filter((day) => {
+  function filter(e, func) {
+    return func().filter((day) => {
       return dateToInt(day.x) >= dateToInt(e.target.from.value) &&
         dateToInt(day.x) <= dateToInt(e.target.to.value)
         ? true
         : false;
     });
   }
+
+  $: type = graphType();
 </script>
 
 <nav>
   <button
     on:click={() => {
-      type = xy();
-      graphType = "xy";
+      graphType = xy;
     }}>PROFIT</button
   >
   <button
     on:click={() => {
-      type = kitty();
-      graphType = "kitty";
+      graphType = kitty;
     }}>KITTY</button
   >
   <button
     on:click={() => {
-      type = log();
-      graphType = "log";
+      graphType = log;
     }}
     style="text-decoration: line-through; opacity: 0.5;">LOG</button
   >
 </nav>
-{#if graphType == "kitty"}
-  <form on:submit={(e) => (type = filterKitty(e))}>
-    <div>
-      <label for="from"> from</label>
-      <input type="date" id="from" value="2023-03-01" />
-    </div>
-    <div>
-      <label for="to"> to</label>
-      <input
-        type="date"
-        id="to"
-        value={new Date().toLocaleDateString("en-CA")}
-      />
-    </div>
-    <button type="submit">Filter</button>
-  </form>
-{/if}
+<form on:submit={(e) => (type = filter(e, graphType))}>
+  <div>
+    <label for="from"> from</label>
+    <input type="date" id="from" value="2023-03-01" />
+  </div>
+  <div>
+    <label for="to"> to</label>
+    <input type="date" id="to" value={new Date().toLocaleDateString("en-CA")} />
+  </div>
+  <button type="submit">Filter</button>
+</form>
 <Chart {type} />
 
 <style>
